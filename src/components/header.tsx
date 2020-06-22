2d4659114de82a887db3d6db51ff6ec1c88338b8
Chris <clbarrell@gmail.com>
2020-06-22 11:49:03 +1000

    added logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as BGV } from "../assets/Vector.svg";
+import icon from "../assets/icon.png";
 // import { ReactComponent as Wave } from "../assets/wave.svg";
 
 export type HeaderProps = { babyName?: string };
@@ -9,11 +10,13 @@ export const Header: React.FC<HeaderProps> = ({ babyName = "" }: HeaderProps) =>
       <div className="flex items-center absolute inset-x-0 top-0 mt-10 text-center">
         <div className="flex-1 text-indigo-100 text-xl">
           <span role="img" aria-label="baby emojy" className="pr-2">
-            👶🏻 
+            👶🏻
           </span>
-           {babyName !== "" ? babyName : "Baby"}
+          {babyName !== "" ? babyName : "Baby"}
         </div>
-        <h1 className="flex-1 text-white font-bold text-5xl">Fed</h1>
+        <h1 className="flex-1 text-white font-bold text-5xl">
+          <img src={icon} alt="Fed icon" className="inline w-12" /> Fed
+        </h1>
       </div>
       <BGV />
       {/* <Wave /> */}
